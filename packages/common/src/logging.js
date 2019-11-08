0947b5e56fbe643d65d43c2232ac5305ce3646e7
Chris Hopkins <chris@liquid-state.com>
2019-11-08 10:28:27 +1000

    Clean up logging and gotoRoute

--- packages/common/src/logging.js
+++ packages/common/src/logging.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const { logger } = window.DD_LOGS || { logger: console.log };
+export const { logger } = window.DD_LOGS || { logger: console };
--- packages/common/src/redux/sagas/app.js
+++ packages/common/src/redux/sagas/app.js
@@ -1,19 +1,11 @@
-import { getContext, takeLatest } from 'redux-saga/effects';
-import { Messages } from '@liquid-state/iwa-core';
+import { call, getContext, takeLatest } from 'redux-saga/effects';
 import { GO_TO_ROUTE } from '../const';
-import { logger } from '../../logging';
 
 export default function* root() {
   yield takeLatest(GO_TO_ROUTE, goToRoute);
 }
 
 function* goToRoute({ payload }) {
-  const msg = Messages.iwa.navigate(payload.route);
-  const app = yield getContext('app');
-  app.communicator.send(msg);
-  logger.log(
-    `event sent: iwa/navigate, route: ${payload.route}`,
-    { type: 'event_sent', event: msg },
-    'debug',
-  );
+  const history = yield getContext('history');
+  yield call(history.push, payload.route);
 }
